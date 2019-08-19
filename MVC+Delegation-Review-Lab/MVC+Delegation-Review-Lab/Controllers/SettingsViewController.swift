2f808290203532c7fcfdd32c5c2607678f8b213e
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-08-19 15:21:04 -0400

    creates new settingsVC coco touch file and sets settings view as settingsVC class. creates outlets for slider, stepper, and label in settings VC

---
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/Controllers/SettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsViewController.swift
+//  MVC+Delegation-Review-Lab
+//
+//  Created by Sunni Tang on 8/19/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var stepper: NSLayoutConstraint!
+    @IBOutlet weak var previewFontLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
